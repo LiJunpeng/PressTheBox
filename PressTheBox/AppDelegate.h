57eab847cdb2438098dba8ab5519425a22a8c947
LILouis <lijunpeng91@gmail.com>
2016-11-08 22:02:08 -0500

    init project git

---
+++ PressTheBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PressTheBox
+//
+//  Created by LILouis on 11/8/16.
+//  Copyright © 2016 LILouis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PressTheBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PressTheBox
+//
+//  Created by LILouis on 11/8/16.
+//  Copyright © 2016 LILouis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
